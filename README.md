7cf7edd4452b5e881b99e7ce7f63ed7f26997052
Nataly Yayloyan <nataly.yayloyan@gmail.com>
2020-05-26 02:54:05 +0400

    draft of portfolio

---
+++ README.md
@@ -0,0 +1,1 @@
+# natalyyayloyan.github.io
---
+++ index.js
@@ -0,0 +1,52 @@
+function openProjects() {
+
+	// let projects = document.getElementById("projectsDIV")).
+	let projects = document.getElementById("projectsDIV");
+	let cv = document.getElementById("CVDIV");
+	let essays = document.getElementById("essaysDIV");
+	if (cv.style.display !== "none") {
+		cv.style.display = "none";
+	}
+	if (essays.style.display !== "none") {
+		essays.style.display = "none";
+	}
+	if (window.getComputedStyle(projects).getPropertyValue('display')=== "none") {
+		projects.style.display = "block";
+	} else {
+		projects.style.display = "none";
+	}
+}
+
+function openEssays() {
+	let projects = document.getElementById("projectsDIV");
+	let cv = document.getElementById("CVDIV");
+	let essays = document.getElementById("essaysDIV");
+	if (cv.style.display !== "none") {
+		cv.style.display = "none";
+	}
+	if (projects.style.display !== "none") {
+		projects.style.display = "none";
+	}
+	if (window.getComputedStyle(essays).getPropertyValue('display') === "none") {
+		essays.style.display = "block";
+	} else {
+		essays.style.display = "none";
+	}
+}
+
+function openResume() {
+	let projects = document.getElementById("projectsDIV");
+	let cv = document.getElementById("CVDIV");
+	let essays = document.getElementById("essaysDIV");
+	if (projects.style.display !== "none") {
+		projects.style.display = "none";
+	}
+	if (essays.style.display !== "none") {
+		essays.style.display = "none";
+	}
+	if (window.getComputedStyle(cv).getPropertyValue('display') === "none") {
+		cv.style.display = "block";
+	} else {
+		cv.style.display = "none";
+	}
+}
